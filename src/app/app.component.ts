ecff14be89c5b29e90530c85aa500c4353f8f95c
Antonio <m167104@adas.co.uk>
2017-01-04 14:27:42 +0000

    Added properly bootstrap CSS and finsih form.
    Forgot that angular CLI needs to change some parameters to use bootsrap. Date input form is different depending browser. Need to use 3d party module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular2 Form Example';
+  title = 'Employee Form';
+  languages = ['English', 'Spanish', 'Other']
 }
