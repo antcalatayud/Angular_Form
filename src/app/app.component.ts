423684739f0e3bff30e948166b414397865c6675
Antonio <m167104@adas.co.uk>
2017-01-05 09:55:34 +0000

    Add Typescript values and add languages enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from './models/employee.model'
+import { Languages } from './models/languages.enum'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Employee Form';
-  languages = ['English', 'Spanish', 'Other']
-  model =  new Employee('','', true, 'low', 'default');
-  hasPrimaryLanguageError = false;
+export class AppComponent implements OnInit {
+  title : string = 'Employee Form';
+  languages : string[] = [];
+  model = new Employee('','', true, 'low', 'default');
+  hasPrimaryLanguageError : boolean = false;
 
-  validatePrimaryLanguage(value){
+  validatePrimaryLanguage(value):void{
     this.hasPrimaryLanguageError = false;
     if(value === 'default')
       this.hasPrimaryLanguageError = true;
+  }
+
+  ngOnInit() {				
+	  this.getLanguages();
+  }
+
+  getLanguages():void{
+      for (var item in Languages) {
+        if (Languages.hasOwnProperty(item)) {
+            this.languages.push(item);
+        }
+     }
   }
 }
